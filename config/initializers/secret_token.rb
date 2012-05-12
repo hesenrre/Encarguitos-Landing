0a19e329f4b4991a9ddb9d2b70fbac2ecc9dce7f
Demetrio Cruz <hesenrre@gmail.com>
2012-05-12 12:58:05 -0500

    init comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Encarguitos::Application.config.secret_token = 'df51d0557818ea5f09327fd9ffee99e6af225b7cee3dec816c400647802775a0c9a382f7539d1da2aee6199cdaaf06e8f8a91201541a64e593829d6b690a96db'
